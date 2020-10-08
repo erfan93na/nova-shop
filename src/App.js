9a393bde773d2b8a65130b7d9e313deb10802e95
erfan93na <62594996+erfan93na@users.noreply.github.com>
2020-10-08 12:37:27 +0330

    first

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import './style.scss';
+
+
+function App() {
+return (
+    <div className="grid-container">
+<header>
+      <a href="#">Nova Shop</a>
+</header>
+      <main>hhhhh
+      </main>
+      <footer>Built with React & Redux.</footer>
+      </div>
+)
+}
+
+export default App
